7d58de66ec0eda88ccc03edb338bf80eb6c65629
Irina Babaeva <irina.a.babaeva@yandex.ru>
2023-07-23 15:18:23 +0200

    page not found error added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -36,7 +36,7 @@ router.use(auth);
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
-router.use('/', (req, res, next) => {
+router.all('*', (req, res, next) => {
   next(new NotFoundError('This page does not exist'));
 });
 
