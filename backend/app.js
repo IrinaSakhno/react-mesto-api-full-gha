30c9331213629e1487f776e810c702316a7fe12d
IrinaSakhno <irina.a.babaeva@yandex.ru>
2023-07-27 15:24:27 +0000

    Mongo DB address updated

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(helmet());
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
